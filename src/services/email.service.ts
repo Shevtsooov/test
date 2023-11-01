376c70ca23291ac1d23aab6ab92a5c7b4883507c
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-11-01 12:06:45 +0200

    added client activatioon

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -10,26 +10,26 @@ const transporter = nodemailer.createTransport({
 });
 
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: 'contact.shevtsov@zohomail.eu', // sender address
-    to: "contact.shevtsov@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+// // async..await is not allowed in global scope, must use a wrapper
+// async function main() {
+//   // send mail with defined transport object
+//   const info = await transporter.sendMail({
+//     from: 'contact.shevtsov@zohomail.eu', // sender address
+//     to: "igorshevtsooov1995@gmail.com", // list of receivers
+//     subject: "Hello ✔", // Subject line
+//     text: "Hello world?", // plain text body
+//     html: "<b>Hello world?</b>", // html body
+//   });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+//   console.log("Message sent: %s", info.messageId);
+//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  //
-  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-  //       <https://github.com/forwardemail/preview-email>
-  //
-};
+//   //
+//   // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+//   //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+//   //       <https://github.com/forwardemail/preview-email>
+//   //
+// };
 
 // main().catch(console.error);
 
@@ -45,6 +45,7 @@ const sendEmail = ({
   html
 }: sendEmail) => {
   return transporter.sendMail({
+    from: "contact.shevtsov@zohomail.eu",
     to: email,
     subject,
     html,
