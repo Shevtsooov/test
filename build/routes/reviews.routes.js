dcd6e83665d3fba6ce0867496d6b36362beb9d09
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-11-18 20:22:32 +0200

    changed everything back

--- build/routes/reviews.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import express from 'express';
-// import {
-//   getGamesList,
-//   addGameToList,
-// } from '../controllers/games.controller'
-// export const gamesRouter = express.Router();
-// gamesRouter.get('/games', express.json(), getGamesList);
-// gamesRouter.post('/games', express.json(), addGameToList);
