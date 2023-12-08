a6d7670112e79f5252e194ae8391295d7fa6e1bf
Shevtsooov <contact.shevtsov@gmail.com>
2023-12-08 17:15:33 +0200

    corrected feedback mail template

--- src/services/emailService/mail_templates/orderStatus/clientOrderCompleted.template.ts
+++ src/services/emailService/mail_templates/orderStatus/clientOrderCompleted.template.ts
@@ -18,6 +18,18 @@ export const generateClientCompletedEmailHTML = (user: IUser) => {
 
         <main style="box-sizing: border-box; display: flex; flex-direction: column; justify-content: center; align-items: center; padding: 10px;">
 
+        <div style="font-size: 16px;">
+          <h3>
+            Вдячні за те, що обрали наші послуги! Ваше задоволення - наш головний пріоритет.
+          </h3>
+
+          <p>
+            Будемо вдячні за ваші теплі слова або зауваження, якщо ви стикалися з якими-небудь труднощами.
+          </p>
+
+          <p>Ваша думка допоможе нам вдосконалити наші послуги.</p>
+        </div>
+
         <a href="${reviewLink}" style="display: block; margin: 25px;">
           <button style="display: block; border: 0; border-radius: 30px; background-color: #0070d1; padding: 10px 25px; font-size: 20px; color: #fff; margin: 0 auto;">
             Залишити відгук
